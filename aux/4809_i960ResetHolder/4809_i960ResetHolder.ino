a861511a0e4e4c9de90a78d438edb6d317ed8016
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-21 11:50:34 -0800

    in the management engine, assert Do Cycle for the entire duration of the actual processing cycle.
    The act of pull MCU_READY low by chipset causes DO_CYCLE to be de-asserted.

--- aux/4809_i960ResetHolder/4809_i960ResetHolder.ino
+++ aux/4809_i960ResetHolder/4809_i960ResetHolder.ino
@@ -297,9 +297,12 @@ transactionBody() noexcept {
     DigitalPin<TRANSACTION_START>::pulse(); // tell the chipset that it can safely pull down the base address of the transaction
     // okay now we need to emulate the wait loop
     do {
-        DigitalPin<DO_CYCLE>::pulse(); // tell the chipset that it is safe to process this data cycle (regardless of where we are)
+        // instead of pulsing do cycle, we just assert do cycle while we wait
+        DigitalPin<DO_CYCLE>::assertPin();
+        //DigitalPin<DO_CYCLE>::pulse(); // tell the chipset that it is safe to process this data cycle (regardless of where we are)
         // now wait for the chipset to tell us it has satisified the current part of the transaction
         while (DigitalPin<MCU_READY>::isDeasserted());
+        DigitalPin<DO_CYCLE>::deassertPin();
         //while (!readyTriggered);
         //readyTriggered = false;
         if (informCPU()) {
