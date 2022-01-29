85d9e658cad40c7ac61e519ee9b135f0f8ba4eef
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-01-28 16:09:24 -0800

    Formatting fixes

--- src/ProcessorSerializer.cc
+++ src/ProcessorSerializer.cc
@@ -26,5 +26,5 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 SplitWord16
 ProcessorInterface::getHalfAddress() noexcept {
-return extractAddress(DigitalPin<i960Pinout::MUXADR0>::readInPort());
+    return extractAddress(DigitalPin<i960Pinout::MUXADR0>::readInPort());
 }
